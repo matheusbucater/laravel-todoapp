704461c6bab6598e6bca0a8be24709eb54017538
matheusbucater <matheusp.bucater@gmail.com>
2021-10-06 00:03:51 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 //        Paginator::useBootstrap();
-        URL::forceScheme('https');
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
