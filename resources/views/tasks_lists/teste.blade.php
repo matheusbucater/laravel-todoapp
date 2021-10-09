210d6bbf02cf3f15676cea1e3d796ab06a649ffd
matheusbucater <matheusp.bucater@gmail.com>
2021-10-08 21:41:37 -0300

    Update teste.blade.php

--- resources/views/tasks_lists/teste.blade.php
+++ resources/views/tasks_lists/teste.blade.php
@@ -9,21 +9,21 @@
 @endsection
 @if($tasks_lists->count() !== 0)
     @section('starred')
-        <div class="container mt-4 justify-content">
+        <div class="container mt-4">
             @foreach($tasks_lists->chunk(3) as $chunk)
             <div class="row mx-auto mb-4">
                 @foreach($chunk as $tasks_list)
-                <div class="bg-white w-25 mx-auto overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="bg-white w-25 h-25 mx-auto overflow-hidden shadow-sm sm:rounded-lg ">
                     <div class="bg-gray-100 text-center pt-2 pb-2">
                         <a href="/tasks-list/{{ $tasks_list->id }}">{{ $tasks_list->title }}</a>
                     </div>
-                    <div class="p-6 bg-white border-b border-gray-200 {!! $tasks_list->tasks->count() !== 0 ? '' : 'text-center' !!}">
-                        <div class="col">
+                    <div class="p-6  bg-white border-b border-gray-200 {!! $tasks_list->tasks->count() !== 0 ? '' : 'text-center' !!} align-text-middle">
+                        <div class="col vertical-center">
                             @if($tasks_list->tasks->count() !== 0)
                                 <div>
                                     <ul>
                                         @foreach($tasks_list->tasks as $task)
-                                            <li>
+                                            <li class="mb-2">
                                                 <span class="px-2 mr-2 inline-flex text-xs leading-5 font-semibold rounded-full {!! $task->completed ? 'bg-green-100' : 'bg-red-100' !!}">
                                                     {!! $task->compled ? 'Finished' : 'Unfinished' !!}
                                                 </span>
@@ -33,9 +33,7 @@
                                     </ul>
                                 </div>
                             @else
-                                <ul>
                                     You don't any tasks.
-                                </ul>
                             @endif
                         </div>
                     </div>
